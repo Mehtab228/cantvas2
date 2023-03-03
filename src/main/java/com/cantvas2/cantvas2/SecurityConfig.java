a1947d6b96597953b93bcde9a5eb0e872c37985d
Joe Rutkin <rutkinj@gmail.com>
2023-03-03 10:27:22 -0800

    initial test passing, home controller test bypasses security

--- src/main/java/com/cantvas2/cantvas2/SecurityConfig.java
+++ src/main/java/com/cantvas2/cantvas2/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cantvas2.cantvas2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile(value = {"development", "production"})
 public class SecurityConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
--- src/test/java/com/cantvas2/cantvas2/HomeControllerTest.java
+++ src/test/java/com/cantvas2/cantvas2/HomeControllerTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.cantvas2.cantvas2.controller.HomeController;
 
-@AutoConfigureMockMvc
+@ActiveProfiles(value = "test")
 @WebMvcTest(HomeController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class HomeControllerTest {
   @Autowired
   private MockMvc mockMvc;
