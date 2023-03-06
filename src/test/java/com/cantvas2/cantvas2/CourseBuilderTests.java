3fd4cdafa2f21ff7af0ff0f5d797c2657f3181e5
Ben Lieberman <bhlieberman93@gmail.com>
2023-03-06 11:12:42 -0700

    unit test added

--- src/test/java/com/cantvas2/cantvas2/CourseBuilderTests.java
+++ src/test/java/com/cantvas2/cantvas2/CourseBuilderTests.java
@@ -2,6 +2,8 @@ package com.cantvas2.cantvas2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDate;
+import static java.time.Month.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,8 @@ public class CourseBuilderTests {
         Course.Builder builder = new Course.Builder();
         assertDoesNotThrow(() -> builder.name("Java 401")
                 .desc("Testing the builder pattern in a Spring app")
+                .startDate(LocalDate.of(2022, OCTOBER, 10))
+                .endDate(LocalDate.of(2022, DECEMBER, 10))
                 .build());
     }
 }
