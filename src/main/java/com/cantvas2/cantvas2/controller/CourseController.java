f6f533e0f02c53ef17536498a65f22a8ce7826f1
Ben Lieberman <bhlieberman93@gmail.com>
2023-03-07 09:13:09 -0700

    working on course enrollment features

--- src/main/java/com/cantvas2/cantvas2/controller/CourseController.java
+++ src/main/java/com/cantvas2/cantvas2/controller/CourseController.java
@@ -6,16 +6,28 @@ import java.time.LocalDate;
 import static java.time.Month.*;
 
 import com.cantvas2.cantvas2.models.Course;
+import com.cantvas2.cantvas2.models.Student;
 
+// import com.cantvas2.cantvas2.services.DatabaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/courses")
 public class CourseController {
+
+  // @Autowired DatabaseService databaseService;
+
   @ModelAttribute
   public void AddCoursesToModel(Model model) {
     List<Course> coursesList = List.of(new Course("Java 401", "Advanced Java course with Spring and Android"),
@@ -48,5 +60,13 @@ public class CourseController {
   @GetMapping("/calendar")
   public String getCourseCalendar() {
     return "courseCalendar";
+  }
+
+  @PutMapping("/enroll/{courseId}")
+  @ResponseBody
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public Student enrollStudent(@RequestBody Student student,
+      @PathVariable(value = "courseId") Long courseId) {
+        return new Student("Ben");
   }
 }
--- src/main/java/com/cantvas2/cantvas2/models/AbstractCantvasUserFactory.java
+++ src/main/java/com/cantvas2/cantvas2/models/AbstractCantvasUserFactory.java
@@ -3,11 +3,14 @@ package com.cantvas2.cantvas2.models;
 public abstract class AbstractCantvasUserFactory {
 
     public Student createStudent(String name) {
-        throw new UnsupportedOperationException("abstract student constructor cannot be invoked directly");
+        throw new UnsupportedOperationException("abstract student constructor cannot be invoked directly.");
     }
 
     public Teacher createTeacher(String name) {
-        throw new UnsupportedOperationException("abstract teacher constructor cannot be invoked directly");
+        throw new UnsupportedOperationException("abstract teacher constructor cannot be invoked directly.");
     }
     
+    public Admin createAdmin(String name) {
+        throw new UnsupportedOperationException("abstract admin constructor cannot be invoked directly.");
+    }
 }
