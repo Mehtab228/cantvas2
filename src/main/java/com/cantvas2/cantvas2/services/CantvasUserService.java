a83bda119ee7727629afe0ab5debfe9c774bf58d
Joe Rutkin <rutkinj@gmail.com>
2023-03-07 16:19:22 -0800

    add method to save student to db, technically works

--- src/main/java/com/cantvas2/cantvas2/services/CantvasUserService.java
+++ src/main/java/com/cantvas2/cantvas2/services/CantvasUserService.java
@@ -1,12 +1,22 @@
 package com.cantvas2.cantvas2.services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import com.cantvas2.cantvas2.models.CantvasUser;
+import com.cantvas2.cantvas2.models.Student;
 
 @Service
 public class CantvasUserService {
   @Autowired JdbcTemplate jdbcTemplate;
 
-  
+  public void saveOne(Student user){
+    jdbcTemplate.update("insert into CANTVAS_USER (username) values (?)", user.getUsername());
+  }
 }
