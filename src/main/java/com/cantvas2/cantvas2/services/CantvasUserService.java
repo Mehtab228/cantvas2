8186426cdb5ac9d6365a29e6674a37292f5e05a5
Joe Rutkin <rutkinj@gmail.com>
2023-03-07 16:23:06 -0800

    add method to get student by id

--- src/main/java/com/cantvas2/cantvas2/services/CantvasUserService.java
+++ src/main/java/com/cantvas2/cantvas2/services/CantvasUserService.java
@@ -9,12 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import com.cantvas2.cantvas2.models.CantvasUser;
 import com.cantvas2.cantvas2.models.Student;
 
 @Service
 public class CantvasUserService {
   @Autowired JdbcTemplate jdbcTemplate;
+
+  public Optional<Student> findById(Long id){
+    List<Student> results = jdbcTemplate.query("select id, username, name where id=?", this::mapRowToStudent, id);
+    return results.size() == 0 ? Optional.empty() : Optional.of(results.get(0));
+  }
+
+  private Student mapRowToStudent(ResultSet resultSet, int rowNumber) throws SQLException {
+      return new Student(
+        resultSet.getString("username"));
+  }
 
   public void saveOne(Student user){
     jdbcTemplate.update("insert into CANTVAS_USER (username) values (?)", user.getUsername());
