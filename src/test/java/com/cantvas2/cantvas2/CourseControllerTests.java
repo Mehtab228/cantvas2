6f373035abfdf203f7f8a54956e869972f140d94
Ben Lieberman <bhlieberman93@gmail.com>
2023-03-08 16:53:00 -0700

    figuring out integration testing and mocking

--- src/test/java/com/cantvas2/cantvas2/CourseControllerTests.java
+++ src/test/java/com/cantvas2/cantvas2/CourseControllerTests.java
@@ -6,40 +6,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import com.cantvas2.cantvas2.config.SecurityConfig;
 import com.cantvas2.cantvas2.models.*;
 import com.cantvas2.cantvas2.services.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Cantvas2Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = { Cantvas2Application.class })
 @AutoConfigureMockMvc(addFilters = false)
 public class CourseControllerTests {
-    
-    @Autowired private MockMvc mockMvc;
 
-    @Autowired DatabaseService databaseService;
+    @Autowired
+    private MockMvc mockMvc;
 
-    StudentFactory studentFactory = new StudentFactory();
-    TeacherFactory teacherFactory = new TeacherFactory();
+    @Autowired
+    private RegistrationService registrationService;
 
-    ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Test
+    @WithMockUser(username = "david", roles = { "ADMIN" })
     void canEnrollStudentUsingAdminRole() throws Exception {
-        Teacher teacher = teacherFactory.createTeacher("David");
-        Student student = studentFactory.createStudent("Ben");
+        Student student = registrationService.studentFactory.createStudent("Ben");
 
         String json = mapper.writeValueAsString(student);
-        String auth = mapper.writeValueAsString(teacher.getAuthorities());
 
         mockMvc.perform(put("/courses/enroll/1")
-        .contentType("application/json")
-        .content(json)
-        .header("X-Authorization", auth)
-        .characterEncoding("utf-8")).andExpect(status().is2xxSuccessful());
+                .contentType("application/json")
+                .content(json)
+                .characterEncoding("utf-8")).andExpect(status().is2xxSuccessful());
     }
 }
